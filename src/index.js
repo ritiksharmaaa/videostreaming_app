384964e059d6ed0e7c705defb6a0519eff488384
ritik sharma <111228529+ritiksharmaaa@users.noreply.github.com>
2024-07-14 20:34:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import express  from "express";
 import connectDB   from "./db/index.js";
   
-dotenv.config({path : './env'})
+dotenv.config({path : './env'});
 
 connectDB()
 
